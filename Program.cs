b6e743d3b4d955c3bc5d514470996a21d1631ed4
Jacob Miller <millerjacob95@gmail.com>
2013-10-15 15:30:44 -0700

    The Start
    Compiler and language start.
    Test sampler in debug folder.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IIgneous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Compile .ii files into .exe");
+                return;
+            }
+
+            try
+            {
+                Scanner scanner = null;
+                using (TextReader input = File.OpenText(args[0]))
+                {
+                    scanner = new Scanner(input);
+                }
+                Parser parser = new Parser(scanner.Tokens);
+                CodeGen codeGen = new CodeGen(parser.Result, Path.GetFileNameWithoutExtension(args[0]) + ".exe");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+    }
+}
